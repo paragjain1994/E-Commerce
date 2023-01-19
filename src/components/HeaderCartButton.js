5aace3fd06bfd5614e14795a94f84a7266a275e1
parag <paragjain528@gmail.com>
2023-01-19 22:30:27 +0530

    HeaderCartButton added

---
+++ src/components/HeaderCartButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = () => {
+  return (
+    <button className={classes.button}>
+      <span>Cart</span>
+      <span className={classes.badge}>0</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
