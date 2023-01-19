e9f5bdba9fcc807ca22692f0ba8a218c95882c5e
parag <paragjain528@gmail.com>
2023-01-19 10:14:09 +0530

    ProductsItem component added for showcasing the products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ProductsItem from "./components/ProductsItem";
+
+const productsArr = [
+  {
+    id: "m1",
+   
+    title: "Album 1",
+
+    price: 100,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+  },
+
+  {
+    id: "m2",
+    
+    title: "Album 2",
+
+    price: 50,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+  },
+
+  {
+    id: "m3",
+    
+    title: "Album 3",
+
+    price: 70,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+  },
+
+  {
+    id: "m4",
+   
+    title: "Album 4",
+
+    price: 100,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+  },
+];
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const productlist = productsArr.map((prod) => (
+    <ProductsItem
+      key={prod.id}
+      id={prod.id}
+      title={prod.title}
+      imageUrl={prod.imageUrl}
+      price={prod.price}
+    ></ProductsItem>
+  ));
+
+  return(
+    <div>
+    <section>
+
+        <ul>{productlist}</ul>
+ 
+    </section>
+  </div>
+  )
+ 
 }
 
 export default App;
