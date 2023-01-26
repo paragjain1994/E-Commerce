7069b5efacaee6f4329114acdc584034272b10c6
parag <paragjain528@gmail.com>
2023-01-26 11:27:11 +0530

    delete button enabled in the Cart items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,67 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
 import classes from "./Cart.module.css";
 import "./Crt.css";
+const DummyCartItems = [
+  {
+    id: "m1",
+
+    title: "Colors",
+
+    price: 100,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+
+    quantity: 2,
+  },
+
+  {
+    id: "m2",
+
+    title: "Black and white Colors",
+
+    price: 50,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+
+    quantity: 3,
+  },
+
+  {
+    id: "m3",
+
+    title: "Yellow and Black Colors",
+
+    price: 70,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+
+    quantity: 1,
+  },
+];
 
 const Cart = (props) => {
+  const [items, updateItems] = useState(DummyCartItems);
+
+  const removeItemFromCart = (event) => {
+    console.log("delete");
+    var list_ = event.target.parentElement;
+    console.log(list_);
+    var id = event.target.parentElement.id;
+    console.log(id);
+
+    console.log(DummyCartItems);
+
+    const idx = items.findIndex((i) => i.id === id);
+    let temp = [...items];
+    temp.splice(idx, 1);
+    updateItems(temp);
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[
-        {
-          title: "Colors",
-
-          price: 100,
-
-          imageUrl:
-            "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-
-          quantity: 2,
-        },
-
-        {
-          title: "Black and white Colors",
-
-          price: 50,
-
-          imageUrl:
-            "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-
-          quantity: 3,
-        },
-
-        {
-          title: "Yellow and Black Colors",
-
-          price: 70,
-
-          imageUrl:
-            "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-
-          quantity: 1,
-        },
-
-        {
-          title: "Black and white Colors",
-
-          price: 50,
-
-          imageUrl:
-            "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-
-          quantity: 3,
-        },
-      ].map((item) => (
-        <li>
-          <div className="cart-row">
-            <span className="cart-item cart-column">
-              <img className="cart-img" src={item.imageUrl} alt="img" />
-              <span className="cart-img">{item.title}</span>
-            </span>
-            <span className="cart-price cart-column">{`$${item.price.toFixed(
-              2
-            )}`}</span>
-            <span className="cart-quantity cart-column">{item.quantity}</span>
-          </div>
+      {items.map((item) => (
+        <li className="cart-row" key={item.id} id={item.id}>
+          <span className="cart-item cart-column">
+            <img className="cart-img" src={item.imageUrl} alt="img" />
+            <span className="cart-img">{item.title}</span>
+          </span>
+          <span className="cart-price cart-column">{`$${item.price.toFixed(
+            2
+          )}`}</span>
+          <span className="cart-quantity cart-column">{item.quantity} </span>
+          <button
+            className="cart-del-btn cart-column"
+            onClick={removeItemFromCart}
+          >
+            x
+          </button>
         </li>
       ))}
     </ul>
@@ -84,8 +98,10 @@ const Cart = (props) => {
         <span>{`$35.7`}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={classes.button}>Purchase</button>
       </div>
     </Modal>
   );
