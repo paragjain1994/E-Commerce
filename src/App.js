58123570e696841aa9ce12436fe7fa992073ebe2
parag <paragjain528@gmail.com>
2023-02-03 17:27:52 +0530

    redirect a page using react router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CartProvider from "./store/CartProvider";
-import { Route } from "react-router-dom";
+import { Route,Redirect } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
 import Store from "./components/Store";
 import Home from "./pages/Home";
@@ -11,20 +11,21 @@ function App() {
   return (
     <CartProvider>
       <Header />
-    
+
       <main>
-        <Route path="/home">
-          <Home />
+      <Route path="/" exact>
+          <Redirect to="/store" />
         </Route>
         <Route path="/store">
           <Store />
+        </Route>
+        <Route path="/home">
+          <Home />
         </Route>
         <Route path="/about">
           <AboutUs />
         </Route>
       </main>
-      
- 
 
       <Footer />
     </CartProvider>
